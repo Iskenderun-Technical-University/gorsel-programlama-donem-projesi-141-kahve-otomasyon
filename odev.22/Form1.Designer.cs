5337d9f3c868551398fe3fbfa9ea7dc83c483f0d
FARESMEZYAD <fares.mezyad.mdbf20@iste.edu.tr>
2022-11-22 13:33:17 +0300

    project added

---
+++ odev.22/Form1.Designer.cs
@@ -0,0 +1,146 @@
+﻿namespace odev._22
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.gunaPictureBox1 = new Guna.UI.WinForms.GunaPictureBox();
+            this.gunaLabel1 = new Guna.UI.WinForms.GunaLabel();
+            this.gunaLabel2 = new Guna.UI.WinForms.GunaLabel();
+            this.la = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.gunaProgressBar1 = new Guna.UI.WinForms.GunaProgressBar();
+            ((System.ComponentModel.ISupportInitialize)(this.gunaPictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // gunaPictureBox1
+            // 
+            this.gunaPictureBox1.BackColor = System.Drawing.Color.Transparent;
+            this.gunaPictureBox1.BaseColor = System.Drawing.Color.White;
+            this.gunaPictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.gunaPictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("gunaPictureBox1.Image")));
+            this.gunaPictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.gunaPictureBox1.Name = "gunaPictureBox1";
+            this.gunaPictureBox1.Radius = 5;
+            this.gunaPictureBox1.Size = new System.Drawing.Size(839, 521);
+            this.gunaPictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.gunaPictureBox1.TabIndex = 0;
+            this.gunaPictureBox1.TabStop = false;
+            this.gunaPictureBox1.Click += new System.EventHandler(this.gunaPictureBox1_Click);
+            // 
+            // gunaLabel1
+            // 
+            this.gunaLabel1.AutoSize = true;
+            this.gunaLabel1.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.gunaLabel1.Font = new System.Drawing.Font("Showcard Gothic", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.gunaLabel1.ForeColor = System.Drawing.Color.SandyBrown;
+            this.gunaLabel1.Location = new System.Drawing.Point(344, 9);
+            this.gunaLabel1.Name = "gunaLabel1";
+            this.gunaLabel1.Size = new System.Drawing.Size(60, 46);
+            this.gunaLabel1.TabIndex = 1;
+            this.gunaLabel1.Text = "I S";
+            this.gunaLabel1.Click += new System.EventHandler(this.gunaLabel1_Click);
+            // 
+            // gunaLabel2
+            // 
+            this.gunaLabel2.AutoSize = true;
+            this.gunaLabel2.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.gunaLabel2.Font = new System.Drawing.Font("Showcard Gothic", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.gunaLabel2.ForeColor = System.Drawing.Color.Gray;
+            this.gunaLabel2.Location = new System.Drawing.Point(399, 9);
+            this.gunaLabel2.Name = "gunaLabel2";
+            this.gunaLabel2.Size = new System.Drawing.Size(71, 46);
+            this.gunaLabel2.TabIndex = 1;
+            this.gunaLabel2.Text = "T E";
+            this.gunaLabel2.Click += new System.EventHandler(this.gunaLabel1_Click);
+            // 
+            // la
+            // 
+            this.la.AutoSize = true;
+            this.la.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.la.Font = new System.Drawing.Font("Stencil", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.la.ForeColor = System.Drawing.Color.SandyBrown;
+            this.la.Location = new System.Drawing.Point(56, 460);
+            this.la.Name = "la";
+            this.la.Size = new System.Drawing.Size(14, 19);
+            this.la.TabIndex = 3;
+            this.la.Text = ".";
+            this.la.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // gunaProgressBar1
+            // 
+            this.gunaProgressBar1.BackColor = System.Drawing.Color.Transparent;
+            this.gunaProgressBar1.BorderColor = System.Drawing.Color.Black;
+            this.gunaProgressBar1.ColorStyle = Guna.UI.WinForms.ColorStyle.Default;
+            this.gunaProgressBar1.IdleColor = System.Drawing.Color.Transparent;
+            this.gunaProgressBar1.Location = new System.Drawing.Point(60, 482);
+            this.gunaProgressBar1.Name = "gunaProgressBar1";
+            this.gunaProgressBar1.ProgressMaxColor = System.Drawing.Color.FromArgb(((int)(((byte)(100)))), ((int)(((byte)(88)))), ((int)(((byte)(255)))));
+            this.gunaProgressBar1.ProgressMinColor = System.Drawing.Color.FromArgb(((int)(((byte)(100)))), ((int)(((byte)(88)))), ((int)(((byte)(255)))));
+            this.gunaProgressBar1.Radius = 5;
+            this.gunaProgressBar1.Size = new System.Drawing.Size(743, 15);
+            this.gunaProgressBar1.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.ClientSize = new System.Drawing.Size(839, 521);
+            this.Controls.Add(this.gunaProgressBar1);
+            this.Controls.Add(this.la);
+            this.Controls.Add(this.gunaLabel2);
+            this.Controls.Add(this.gunaLabel1);
+            this.Controls.Add(this.gunaPictureBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.gunaPictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Guna.UI.WinForms.GunaPictureBox gunaPictureBox1;
+        private Guna.UI.WinForms.GunaLabel gunaLabel1;
+        private Guna.UI.WinForms.GunaLabel gunaLabel2;
+        private System.Windows.Forms.Label la;
+        private System.Windows.Forms.Timer timer1;
+        private Guna.UI.WinForms.GunaProgressBar gunaProgressBar1;
+    }
+}
+
---
+++ odev.22/Form1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace odev._22
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        int a = 0;
+        private void gunaLabel1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void gunaPictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+
+        }
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (a == 0)
+                la.Text = " Loading.....";
+            if (a == 20)
+                la.Text = " FARES MEZYAD";
+            if (a == 60)
+                la.Text = " Ve..OSAMA ALSAIDI  ";
+            if (a == 70)
+                la.Text = " Ve.. MEHHEMET ÇATAKAÇIN  ";
+            if (a == 80)
+                la.Text = " MUTLU GÜNLER DİLERLER.....";
+            a += 1;
+            if (a >= 100)
+            {
+                this.Hide();
+                timer1.Enabled = false;
+                Form2 f2 = new Form2();
+                f2.ShowDialog();
+            }
+            gunaProgressBar1.Value = a;
+
+
+
+
+        }
+    }
+}
